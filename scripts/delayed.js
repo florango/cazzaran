af170dd49c5f425625b13af2ab9881f9855f4010
Joshua Oransky <Josh@Cazzaran.com>
2023-06-29 20:50:54 -0700

    Added Google Analytics

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,22 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { createEl } from './scripts.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+createEl('script', {
+    src: 'https://www.googletagmanager.com/gtag/js?id=G-M0997FK1N2',
+    async: true,
+}, '', document.head);
+
+createEl('script', {
+}, `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-M0997FK1N2');
+`, document.head);
